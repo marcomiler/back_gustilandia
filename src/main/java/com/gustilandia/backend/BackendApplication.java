d6035499dbaa6a7aa83798bc2250d1e973328d5e
marcomiler <miler.rmarco@gmail.com>
2021-05-17 02:52:11 -0500

    tercer commit

--- src/main/java/com/gustilandia/backend/BackendApplication.java
+++ src/main/java/com/gustilandia/backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BackendApplication extends SpringBootServletInitializer{
@@ -16,5 +17,12 @@ public class BackendApplication extends SpringBootServletInitializer{
 	@Bean
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(BackendApplication.class);
+
 	}
 }
